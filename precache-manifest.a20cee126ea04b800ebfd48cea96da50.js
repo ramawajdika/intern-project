4d2a65f670f0cdec46f395aef302391ff35e9e48
ramawajdika <ramawajdi.anwar@gmail.com>
2019-04-15 17:42:24 +0700

    Updates

---
+++ precache-manifest.a20cee126ea04b800ebfd48cea96da50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3535eeca54d580a6962061d65d3b5fe",
+    "url": "/intern-project/static/media/logo.f3535eec.svg"
+  },
+  {
+    "revision": "d212e7b7dd094ee305704c56f5fcafce",
+    "url": "/intern-project/static/media/bg.d212e7b7.png"
+  },
+  {
+    "revision": "99ea4409ccde9a7d1ee9",
+    "url": "/intern-project/static/js/runtime~main.cc3e4a94.js"
+  },
+  {
+    "revision": "2c30336259e7a58dc804",
+    "url": "/intern-project/static/js/main.0a299b61.chunk.js"
+  },
+  {
+    "revision": "9ccdfd62441dcd58d060",
+    "url": "/intern-project/static/js/2.671fa6e9.chunk.js"
+  },
+  {
+    "revision": "2c30336259e7a58dc804",
+    "url": "/intern-project/static/css/main.f153131b.chunk.css"
+  },
+  {
+    "revision": "f9a93ead3a55ceb108c3c306c8b2c79c",
+    "url": "/intern-project/index.html"
+  }
+];
